a45cf8be482c92c548a6f2a8c22997c23130ce7b
asyakovlev <asyakovlev@navio.auto>
2025-11-24 02:39:09 +0300

    feat: add LinkedIn profile and update interests in CV; enhance PDF generation and layout

--- src/make-pdf.js
+++ src/make-pdf.js
@@ -3,10 +3,18 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://aleksandr-yakovlev.github.io/cv.html", {
+  await page.setViewport({ width: 794, height: 1123, deviceScaleFactor: 1 });
+  await page.goto("http://localhost:3000/cv.html", {
     waitUntil: "networkidle2",
   });
-  await page.pdf({ path: "./cv.pdf", scale: 0.69, printBackground: true });
+  await page.evaluateHandle("document.fonts.ready");
+  await page.emulateMediaType("screen");
+  await page.pdf({
+    path: "./cv.pdf",
+    format: "A4",
+    printBackground: true,
+    margin: { top: 0, bottom: 0, left: 0, right: 0 },
+  });
 
   await browser.close();
 })();
